bc37c63bcb8f664725814f294888814b24c8e0db
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-02-11 20:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    console.log("Just got a request!");
+
     async function hitURL() {
-    const response = await fetch('https://text-to-speech-uajn.onrender.com/')
-        const movies = await response.json();
-        console.log(movies);
-  }
+        try {
+            const response = await fetch('https://text-to-speech-uajn.onrender.com/');
+            const movies = await response.json();
+            console.log(movies);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
   
-  // Call the function immediately
-  hitURL();
+    // Call the function immediately
+    hitURL();
   
-  // Call the function every 30 minutes (30 minutes = 30 * 60 * 1000 milliseconds)
-  setInterval(hitURL, 30 );
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+    // Call the function every 30 minutes
+    setInterval(hitURL, 30);
+  
+    res.send('Yo!');
+});
+
+app.listen(process.env.PORT || 3000);
